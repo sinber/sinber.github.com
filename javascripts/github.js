40ccaff7c204bfeb1cb629cef9dfe224e78f08ef
sinber <sinber@126.com>
2013-06-12 11:13:09 +0800

    Site updated at 2013-06-12 03:13:08 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,3 +1,40 @@
+function getElementsByClassName(node,classname) {
+  if (node.getElementsByClassName) { // use native implementation if available
+    return node.getElementsByClassName(classname);
+  } else {
+    return (function getElementsByClass(searchClass,node) {
+        if ( node == null )
+          node = document;
+        var classElements = [],
+            els = node.getElementsByTagName("*"),
+            elsLen = els.length,
+            pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)"), i, j;
+
+        for (i = 0, j = 0; i < elsLen; i++) {
+          if ( pattern.test(els[i].className) ) {
+              classElements[j] = els[i];
+              j++;
+          }
+        }
+        return classElements;
+    })(classname, node);
+  }
+}
+
+function toggle_visibility(className) {
+   var elements = getElementsByClassName(document, className),
+       n = elements.length;
+   for (var i = 0; i < n; i++) {
+     var e = elements[i];
+
+     if(e.style.display == 'block') {
+       e.style.display = 'none';
+     } else {
+       e.style.display = 'block';
+     }
+  }
+}
+
 var github = (function(){
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
